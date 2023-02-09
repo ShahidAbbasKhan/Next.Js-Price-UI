9d6f367bd4c217b7d865c78448ec880573194ce3
ShahidAbbasKhan <developershahid297@gmail.com>
2023-02-09 17:43:00 +0500

    updating formatting

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,19 @@
-import { Box,Heading,Text} from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function Header() {
   return (
-    <Box textAlign={{base:"left", md:"left",lg:"center"}} bg="#6B46C1" pb="240px" pt="90px" pl="10px" color="white">
-        <Heading pb="14px" mt="10px">
-          Simple pricing for your business
-        </Heading>
-        <Text>
-          Plans that are carefully crafted to suit your business.
-        </Text>
+    <Box
+      textAlign={{ base: "left", md: "left", lg: "center" }}
+      bg="#6B46C1"
+      pb="240px"
+      pt="90px"
+      pl="10px"
+      color="white"
+    >
+      <Heading pb="14px" mt="10px">
+        Simple pricing for your business
+      </Heading>
+      <Text>Plans that are carefully crafted to suit your business.</Text>
     </Box>
-  )
+  );
 }
- 
--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,49 +1,67 @@
-import { Box, Button, Flex,Heading,HStack,Text,Icon } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
 import firstIcon from "../Icons/firstIcon";
-import React from 'react'
+import React from "react";
 
 export default function Price() {
   return (
-    <Box bg={"#FFFFFF"} borderRadius="10px"  overflow={"hidden"} maxW="994px" ml={{base:"10px",md:"20px", lg:"auto"}} mr={{base:"10px", md:"20px", lg:"auto"}} mt="-160" boxShadow={
-        "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);"}>
-        <Flex direction={{base:"column", md:"column", lg:"row"}}>
-        <Box bg={"#F0EAFB"}  p="50px" textAlign={"center"} >
-            <Text fontWeight={"bold"} fontSize="24px">
-                Premium PRO
-            </Text>
-            <Heading  fontSize={"60px"} mt="10px">
-                $329
-            </Heading>
-            <Text mb="15px" mt="6px">
-                billed just once
-            </Text>
-            <Button color="white" bg="#805AD5"  w="270px">
-                Get Started
-            </Button>
-
+    <Box
+      bg={"#FFFFFF"}
+      borderRadius="10px"
+      overflow={"hidden"}
+      maxW="994px"
+      ml={{ base: "10px", md: "20px", lg: "auto" }}
+      mr={{ base: "10px", md: "20px", lg: "auto" }}
+      mt="-160"
+      boxShadow={
+        "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);"
+      }
+    >
+      <Flex direction={{ base: "column", md: "column", lg: "row" }}>
+        <Box bg={"#F0EAFB"} p="50px" textAlign={"center"}>
+          <Text fontWeight={"bold"} fontSize="24px">
+            Premium PRO
+          </Text>
+          <Heading fontSize={"60px"} mt="10px">
+            $329
+          </Heading>
+          <Text mb="15px" mt="6px">
+            billed just once
+          </Text>
+          <Button color="white" bg="#805AD5" w="270px">
+            Get Started
+          </Button>
         </Box>
         <Box p="48px">
-            <Text mb="20px">
-               Access these features when you get this pricing package for your business.
-            </Text>
-            <HStack>
-                <Icon as={firstIcon} />
-                <Text>International calling and messaging API</Text>
-            </HStack>
-            <HStack mt="14px">
-                <Icon as={firstIcon} />
-                <Text>Additional phone numbers</Text>
-            </HStack>
-            <HStack mt="14px">
-                <Icon as={firstIcon} />
-                <Text>Automated messages via Zapier</Text>
-            </HStack>
-            <HStack mt="14px">
-                <Icon as={firstIcon} />
-                <Text>24/7 support and consulting</Text>
-            </HStack>
+          <Text mb="20px">
+            Access these features when you get this pricing package for your
+            business.
+          </Text>
+          <HStack>
+            <Icon as={firstIcon} />
+            <Text>International calling and messaging API</Text>
+          </HStack>
+          <HStack mt="14px">
+            <Icon as={firstIcon} />
+            <Text>Additional phone numbers</Text>
+          </HStack>
+          <HStack mt="14px">
+            <Icon as={firstIcon} />
+            <Text>Automated messages via Zapier</Text>
+          </HStack>
+          <HStack mt="14px">
+            <Icon as={firstIcon} />
+            <Text>24/7 support and consulting</Text>
+          </HStack>
         </Box>
-        </Flex>
+      </Flex>
     </Box>
-  )
+  );
 }
